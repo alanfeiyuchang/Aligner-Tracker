705dd60d766e2150a75954734ad77fa23373d5e0
Feiyu Chang <alanfeiyuchang@gmail.com>
2026-06-02 19:43:45 -0700

    Initial Commit

---
+++ Aligner Tracker/Aligner_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aligner_TrackerApp.swift
+//  Aligner Tracker
+//
+//  Created by ChangFeiyu on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Aligner_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
